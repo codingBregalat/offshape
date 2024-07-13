e00cd8bb50a5965cdd605e4ffb8aec2da6c9b9ae
codingBregalat <147812202+codingBregalat@users.noreply.github.com>
2024-07-13 08:40:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// script.js
+
+// Function to detect if the user is on a mobile device
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+// Change video source based on device type
+window.onload = function() {
+    var video = document.getElementById('index-video');
+    var source = document.getElementById('video-source');
+    if (isMobileDevice()) {
+        source.src = 'landingpageVideoMobilecompressed.mp4';
+        video.load();
+    }
+};
+
+// Add smooth scroll behavior to all anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
