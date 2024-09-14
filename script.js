cfbf3e8b96a2698215ccbae0c7cf1386b9e714ae
codingBregalat <147812202+codingBregalat@users.noreply.github.com>
2024-09-14 12:41:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 
-// Change video source based on device type
 window.onload = function() {
     var video = document.getElementById('index-video');
     var source = document.getElementById('video-source');
+    
+    // Check if it's a mobile device and adjust the video source
     if (isMobileDevice()) {
-        source.src = 'Logo revealmobile.mp4';
-        video.setAttribute('playsinline', ''); // Add playsinline attribute
-        video.load();
+        source.src = 'Logo revealmobilecompressed.mp4'; // Use mobile-optimized video
+    } else {
+        source.src = 'website versioncompressed.mp4'; // Use desktop version video
     }
+
+    video.setAttribute('muted', ''); // Ensure video is muted for autoplay to work on mobile
+    video.setAttribute('playsinline', ''); // Mobile video to play inline
+    video.setAttribute('autoplay', ''); // Ensure autoplay works
+    video.setAttribute('loop', ''); // Ensure video loops
+
+    video.load(); // Reload the video with the updated source
 };
 
 // Add smooth scroll behavior to all anchor links
